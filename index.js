d641675758b6804805c4ffad77c74ed8dac8be48
witty-collection-3981 <yisroel96@gmail.com>
2018-10-09 17:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function theBeatlesPlay(musicians, instruments) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
+  return array
+}
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const array = []
+
+  do {
+    array.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return array
+}
